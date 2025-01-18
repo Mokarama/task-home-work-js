e1fc898b27b522117c4dedc419bf84be846b74f1
Mokarama <mokaramaaktershanta@gmail.com>
2025-01-18 19:08:03 +0600

    last commit

--- problem-10.js
+++ problem-10.js
@@ -5,27 +5,7 @@
 //  Implement a countdown timer that counts down from 81 to 65.
 //  let count = 81;
 
-
-let sum =0;
 for(let i=81; i>=65; i--){
     console.log(i);
 }
 
-
-
-
-
-
-
-
-
-
-
-// const countdown = setInterval(() => {
-//     console.log(Countdown: ${count});
-//     count--;
-//     if (count < 65) {
-//         clearInterval(countdown);
-//         console.log("Countdown finished!");
-//     }
-// }, 1000);
